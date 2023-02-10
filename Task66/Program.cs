dfd9db908de1c6694ada7111942af14541ee0b23
Sarmaon <patratiyaa@gmail.com>
2023-02-10 17:50:38 +0800

    init, add tasks

---
+++ Task66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+//элементов в промежутке от M до N.
+//Выполнить с помощью рекурсии.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M ");
+int NumberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+
+int result = SummNumbers(NumberM, NumberN);
+
+        if (NaturalNum(NumberM, NumberN))
+            Console.WriteLine(result);
+        else
+            Console.WriteLine("Ошибка ввода");
+
+int SummNumbers(int num1, int num2)
+{
+    if (num1 < num2) return num1 + SummNumbers(num1 + 1, num2);
+    if (num1 > num2) return num1 + SummNumbers(num1 - 1, num2);
+    if (num1 == num2) return num1;
+    return num1;
+
+}
+
+bool NaturalNum(int num1, int num2)
+    {
+    return (num1 > 0 && num2 > 0);
+    }
---
+++ Task68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+//С учетом условия, нет смысла прописывать юзеру необходимость ввода положительных чисел.
+
+Console.WriteLine("Введите число M ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N ");
+int numberN = int.Parse(Console.ReadLine());
+
+int FunAkk = FunAkkerman(numberM, numberN);
+if (numberM >=0 && numberN >=0)
+Console.WriteLine($"m = {numberM}, n = {numberN} -> A(m,n) = {FunAkk}");
+
+int FunAkkerman(int numM, int numN)
+{
+    if (numM == 0)
+    return numN + 1;
+        else if ((numM != 0) && (numN == 0))
+        return FunAkkerman(numM - 1, 1);
+            if ((numM > 0) && (numN > 0))
+            return FunAkkerman(numM - 1, FunAkkerman(numM, numN - 1));
+return 0;
+}
